2befd4020d8e78f5886d37157e76eefeef072d42
Carsten Saathoff <carsten@kreuzverweis.com>
2012-11-06 10:48:52 +0100

    fixed build to publish artifacts correctly

--- build.sbt
+++
@@ -1,9 +0,0 @@
-organization := "net.databinder"
-
-version := "0.6.4"
-
-scalaVersion := "2.9.2"
-
-// Publishing to private repository server for now.
-publishTo := Some(Resolver.file("repository", new File("/var/www/repository")))
-
--- project/build.scala
+++ project/build.scala
@@ -2,24 +2,37 @@ import sbt._
 import Keys._
 
 object UnfilteredOAuth2Build extends Build {
-  lazy val root = Project(id = "unfiltered-oauth2-root", base = file(".")) aggregate(mac, oauth2)
+  def defaultSettings: Seq[Setting[_]] = Seq(
+		organization := "net.databinder",
+		version := "0.6.4",
+		scalaVersion := "2.9.2",
+		publishTo := repo
+	)
 
-  lazy val mac = Project(id = "unfiltered-mac", base = file("mac")).settings(
-    libraryDependencies ++= Seq(
-    	"net.databinder" %% "unfiltered" % "0.6.4",
-    	"net.databinder" %% "unfiltered-spec" % "0.6.4",
-    	"org.scala-tools.testing" % "specs_2.9.1" % "1.6.9" % "test"
-    )
-  )
+
+  val repo = Some(Resolver.file("repository", new File("/tmp")))
+
+  lazy val root = Project(id = "unfiltered-oauth2-root", base = file("."))
+  	.settings(defaultSettings :_*)
+  	.aggregate(mac, oauth2)
+
+  lazy val mac = Project(id = "unfiltered-mac", base = file("mac"))
+  	.settings(defaultSettings ++ Seq(
+    		libraryDependencies ++= Seq(
+    			"net.databinder" %% "unfiltered" % "0.6.4",
+    			"net.databinder" %% "unfiltered-spec" % "0.6.4",
+    			"org.scala-tools.testing" % "specs_2.9.1" % "1.6.9" % "test"
+    		)) :_*
+  	)
 
   lazy val oauth2 = Project(id = "unfiltered-oauth2", base = file("oauth2"))
   	.dependsOn(mac)
-  	.settings(
+  	.settings(defaultSettings ++ Seq(
 		libraryDependencies ++= Seq(
 			"net.databinder" %% "unfiltered-filter" % "0.6.4",
 			"net.databinder" %% "unfiltered-jetty" % "0.6.4",
 			"net.databinder" %% "unfiltered-spec" % "0.6.4",
 			"org.scala-tools.testing" % "specs_2.9.1" % "1.6.9" % "test"
-		)
+		)) :_*
 	)
 }
